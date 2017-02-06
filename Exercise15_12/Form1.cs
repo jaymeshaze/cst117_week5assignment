bb3493000c20ae1984563d49b1da7316667fdc16
jaymeshaze <jaymeshaze@outlook.com>
2017-02-06 00:12:45 -0700

    Updated exercise 15_12

--- Exercise15_12/Form1.cs
+++ Exercise15_12/Form1.cs
@@ -50,28 +50,59 @@ namespace Exercise15_12
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            cmd.CommandText = "SELECT Balance "
-            + "FROM Accounts WHERE AccountNumber = " + listBox1.SelectedItem.ToString();
-            reader = cmd.ExecuteReader();
-            textBox2.Text = Convert.ToString(reader.GetDecimal(1));
+
         }
 
         private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            textBox3.Text = listBox2.SelectedItem.ToString();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
-            //double difBalance = 0;
-            //try
-            //{
-            //    String transferBalance = textBox1.Text;
-            //    difBalance = (Convert.ToDouble(transferBalance)) + ;
-            //}catch(FormatException f) { }
-            //cmd.CommandText = "UPDATE Accounts SET Balance = " + difBalance + " WHERE AccountNumber = 1111";
-            //cmd.ExecuteNonQuery();
+            decimal transferAmount = 0;
+            try
+            {
+                String transferBalance = textBox1.Text;
+                transferAmount = (Convert.ToDecimal(transferBalance));
+            }
+            catch (FormatException f) { }
+
+            decimal receivingCurrentBalance = 0;
+            try
+            {
+                cmd.CommandText = "SELECT Balance "
+            + "FROM Accounts WHERE AccountNumber = " + listBox1.SelectedItem.ToString();
+                reader = cmd.ExecuteReader();
+                receivingCurrentBalance = reader.GetDecimal(1); //error pulling in current balance, need to fix
+                
+            }catch(InvalidOperationException g) { }
+
+            reader.Close();
+
+            decimal sendingCurrentBalance = 0;
+            try
+            {
+                cmd.CommandText = "SELECT Balance "
+                + "FROM Accounts WHERE AccountNumber = " + listBox2.SelectedItem.ToString();
+                reader = cmd.ExecuteReader();
+                sendingCurrentBalance = reader.GetDecimal(1); //error pulling in current balance, need to fix
+            }catch(InvalidOperationException h) { }
+            
+
+
+            reader.Close();
+
+            decimal sendingNewBalance = sendingCurrentBalance - transferAmount;
+            decimal ReceivingNewBalance = receivingCurrentBalance + transferAmount;
+
+            //Updates do work and post correct value, need to correct pull of current account balance above to complete
+            cmd.CommandText = "UPDATE Accounts SET Balance = " + ReceivingNewBalance + " WHERE AccountNumber = " + listBox2.SelectedItem.ToString();
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "UPDATE Accounts SET Balance = " + sendingNewBalance + " WHERE AccountNumber = " + listBox1.SelectedItem.ToString();
+            cmd.ExecuteNonQuery();
         }
     }
 
